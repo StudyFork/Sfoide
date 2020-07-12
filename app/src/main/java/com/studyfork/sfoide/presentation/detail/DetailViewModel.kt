37b7c2534514bb84a40f87d07b6c0fa476495b66
dlwls5201 <dlwls5201@naver.com>
2020-07-12 17:23:07 +0900

    add phone, cell, email function

--- app/src/main/java/com/studyfork/sfoide/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/studyfork/sfoide/presentation/detail/DetailViewModel.kt
@@ -1,5 +1,8 @@
 package com.studyfork.sfoide.presentation.detail
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -11,6 +14,20 @@ import com.studyfork.sfoide.presentation.model.UserItem
 class DetailViewModel(
     val userItem: UserItem
 ) : BaseViewModel(), OnMapReadyCallback {
+
+    fun goToCall(context: Context) {
+        context.startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + userItem.cell)))
+    }
+
+    fun goToPhone(context: Context) {
+        context.startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + userItem.phone)))
+    }
+
+    fun goToEmail(context: Context) {
+        val email = Intent(Intent.ACTION_SEND);
+        email.type = "plain/Text";
+        context.startActivity(email)
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         userItem.coordinates.let {
