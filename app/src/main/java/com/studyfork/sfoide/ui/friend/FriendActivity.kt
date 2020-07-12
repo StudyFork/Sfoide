def3ac4a32631281be5caedaa10584740b62b363
improve777 <improve777@gmail.com>
2020-07-12 14:52:41 +0900

    FriendViewModel Factory 추가

--- app/src/main/java/com/studyfork/sfoide/ui/friend/FriendActivity.kt
+++ app/src/main/java/com/studyfork/sfoide/ui/friend/FriendActivity.kt
@@ -1,18 +1,29 @@
 package com.studyfork.sfoide.ui.friend
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.studyfork.sfoide.R
+import com.studyfork.sfoide.data.datasource.RemoteFriendDataSourceImpl
+import com.studyfork.sfoide.data.remote.RetrofitService
 import com.studyfork.sfoide.databinding.ActivityFriendBinding
 
 class FriendActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFriendBinding
 
-    private val viewModel: FriendViewModel by viewModels()
+    private val viewModel: FriendViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return FriendViewModel(RemoteFriendDataSourceImpl(RetrofitService.friendApi)) as T
+            }
+        }
+    }
 
     private lateinit var friendAdapter: FriendAdapter
 
@@ -27,7 +38,7 @@ class FriendActivity : AppCompatActivity() {
 
     private fun initRecyclerView() {
         friendAdapter = FriendAdapter(viewModel)
-        binding.rvMain.adapter =  friendAdapter
+        binding.rvMain.adapter = friendAdapter
     }
 
     private fun observeViewModel() {
