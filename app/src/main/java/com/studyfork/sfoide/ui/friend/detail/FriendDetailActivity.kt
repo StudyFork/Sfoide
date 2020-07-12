c2368d924af54390ccc6572df86fd1a4d3813ec1
improve777 <improve777@gmail.com>
2020-07-12 16:38:42 +0900

    FriendDetailActivity 에 클릭 이벤트 추가

--- app/src/main/java/com/studyfork/sfoide/ui/friend/detail/FriendDetailActivity.kt
+++ app/src/main/java/com/studyfork/sfoide/ui/friend/detail/FriendDetailActivity.kt
@@ -2,7 +2,9 @@ package com.studyfork.sfoide.ui.friend.detail
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,10 +16,13 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.studyfork.sfoide.R
 import com.studyfork.sfoide.data.model.Friend
 import com.studyfork.sfoide.databinding.ActivityFriendDetailBinding
+import com.studyfork.sfoide.ui.utils.EventObserver
 
 class FriendDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityFriendDetailBinding
+
+    private val viewModel: FriendDetailViewModel by viewModels()
 
     private var friend: Friend? = null
 
@@ -25,8 +30,10 @@ class FriendDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_friend_detail)
         binding.lifecycleOwner = this
+        binding.vm = viewModel
 
         initMap()
+        observeViewModel()
 
         friend = intent.getParcelableExtra<Friend>(EXTRA_FRIEND)
         binding.friend = this.friend
@@ -36,6 +43,31 @@ class FriendDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment =
             (supportFragmentManager.findFragmentById(R.id.map_friend_detail) as? SupportMapFragment)
         mapFragment?.getMapAsync(this)
+    }
+
+    private fun observeViewModel() {
+        with(viewModel) {
+            navigatePhoneAppEvent.observe(
+                this@FriendDetailActivity,
+                EventObserver(this@FriendDetailActivity::openDialApp)
+            )
+
+            navigateEmailAppEvent.observe(
+                this@FriendDetailActivity,
+                EventObserver(this@FriendDetailActivity::openEmailApp)
+            )
+        }
+    }
+
+    private fun openDialApp(phone: String) {
+        startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone")))
+    }
+
+    private fun openEmailApp(email: String) {
+        startActivity(Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, email)
+        })
     }
 
     override fun onMapReady(map: GoogleMap?) {
