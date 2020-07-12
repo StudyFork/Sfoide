afb04f4c379d1e66ce03c610ca59cbeb66217102
improve777 <improve777@gmail.com>
2020-07-12 15:56:54 +0900

    FriendDetailActivity 에 구글맵 api 연동하여 좌표 표시

--- app/src/main/java/com/studyfork/sfoide/ui/friend/detail/FriendDetailActivity.kt
+++ app/src/main/java/com/studyfork/sfoide/ui/friend/detail/FriendDetailActivity.kt
@@ -5,26 +5,54 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.studyfork.sfoide.R
 import com.studyfork.sfoide.data.model.Friend
 import com.studyfork.sfoide.databinding.ActivityFriendDetailBinding
 
-class FriendDetailActivity : AppCompatActivity() {
+class FriendDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityFriendDetailBinding
+
+    private var friend: Friend? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_friend_detail)
         binding.lifecycleOwner = this
 
-        val friend = intent.getParcelableExtra<Friend>(EXTRA_FRIEND)
-        binding.friend = friend
+        initMap()
+
+        friend = intent.getParcelableExtra<Friend>(EXTRA_FRIEND)
+        binding.friend = this.friend
+    }
+
+    private fun initMap() {
+        val mapFragment =
+            (supportFragmentManager.findFragmentById(R.id.map_friend_detail) as? SupportMapFragment)
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        friend?.coordinates?.let {
+            val coordinates = LatLng(it.latitude, it.longitude)
+            map?.addMarker(
+                MarkerOptions()
+                    .position(coordinates)
+            )
+
+            map?.moveCamera(CameraUpdateFactory.newLatLng(coordinates))
+        }
     }
 
     companion object {
-        const val EXTRA_FRIEND = "EXTRA_FRIEND"
 
+        const val EXTRA_FRIEND = "EXTRA_FRIEND"
         fun createIntent(context: Context, friend: Friend): Intent {
             return Intent(context, FriendDetailActivity::class.java).apply {
                 putExtra(EXTRA_FRIEND, friend)
