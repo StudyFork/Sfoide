71848a00f75c5a067b1a0fc6eaaff0dd8771c830
ahndwon <gahlok24@gmail.com>
2020-08-09 21:49:08 +0900

    add base binding fragment

--- app/src/main/java/com/studyfork/sfoide/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/studyfork/sfoide/presentation/base/BaseFragment.kt
@@ -2,17 +2,32 @@ package com.studyfork.sfoide.presentation.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment(@LayoutRes private val layoutResId: Int) : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(layoutResId, container, false)
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+
+    protected lateinit var binding: B
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBinding()
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.inflate<B>(
+            LayoutInflater.from(requireContext()),
+            layoutResId,
+            binding.root.parent as ViewGroup,
+            false
+        ).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
     }
 }
--- app/src/main/java/com/studyfork/sfoide/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/studyfork/sfoide/presentation/home/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.studyfork.sfoide.R
+import com.studyfork.sfoide.databinding.FragmentHomeBinding
 import com.studyfork.sfoide.presentation.base.BaseFragment
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel by viewModels<HomeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.viewModel = viewModel
         viewModel.getRandomUsers()
     }
 }
