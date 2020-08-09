26150d6a2902e783cd1b552f09644967b84c3ebc
ahndwon <gahlok24@gmail.com>
2020-08-10 01:58:25 +0900

    Add double back press finish

--- app/src/main/java/com/studyfork/sfoide/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/studyfork/sfoide/presentation/base/BaseActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 
-abstract class BaseActivity(@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
+abstract class BaseActivity(@LayoutRes private val layoutResId: Int) : BackPressActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutResId)
